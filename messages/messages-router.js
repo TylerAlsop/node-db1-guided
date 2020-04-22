0a42244250430263dedd6e468e4742b1fa184b5b
Tyler Alsop <tylerjalsop@gmail.com>
2020-04-21 19:05:45 -0600

    Initial Commit. Installed knex, sqlite3. Completed first GET req for all messages.

--- messages/messages-router.js
+++ messages/messages-router.js
@@ -3,23 +3,40 @@ const db = require("../data/config")
 
 const router = express.Router()
 
-router.get("/", (req, res, next) => {
+
+/////////////// GET ///////////////
+
+router.get("/", async (req, res, next) => {
+    try {
+        // SQL Command: SELECT * FROM "messages"
+        const messages = await db.select("*").from("messages")
+        res.json(messages)
+    } catch (err) {
+        next(err)
+    }
 
 })
 
-router.get("/:id", (req, res, next) => {
+router.get("/:id", async (req, res, next) => {
 
 })
 
-router.post("/", (req, res, next) => {
+
+/////////////// POST ///////////////
+
+router.post("/", async (req, res, next) => {
 
 })
 
-router.put("/:id", (req, res, next) => {
+/////////////// PUT ///////////////
+
+router.put("/:id", async (req, res, next) => {
 
 })
 
-router.delete("/:id", (req, res, next) => {
+/////////////// DELETE ///////////////
+
+router.delete("/:id", async (req, res, next) => {
 
 })
 
